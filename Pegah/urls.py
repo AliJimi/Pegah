c97d31efad9e1f8ba767b6842e19e20b19d4c379
Ali Jimi <najimi@ce.sharif.edu>
2023-02-03 12:41:44 +0330

    some changes

--- Pegah/urls.py
+++ Pegah/urls.py
@@ -21,5 +21,5 @@ from Pegah.settings import MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cafe/', include('cafe.urls')),
+    path('cafe/', include('cafe.urls', namespace='cafe')),
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
--- cafe/urls.py
+++ cafe/urls.py
@@ -4,16 +4,19 @@ from cafe.views.main import landing_view, main_view
 from cafe.views.menu import menu_view, menu_courses_api, add_course, CourseCreateView
 from cafe.views.reservation import reservation_view
 
+
+app_name = "cafe"
+
 urlpatterns = [
     path('', main_view,),
-    path('menu/', menu_view, name='cafe-menu'),
-    path('menu/courses/api/', menu_courses_api, name='cafe-menu-courses-api'),
+    path('menu/', menu_view, name='menu'),
+    path('menu/courses/api/', menu_courses_api, name='menu-courses-api'),
 
-    path('menu/courses/', menu_courses_api, name='cafe-menu-courses'),
-    path('menu/courses/add/', add_course, name='cafe-menu-courses-add'),
-    path('landing/', landing_view, name='cafe-landing'),
+    path('menu/courses/', menu_courses_api, name='menu-courses'),
+    path('menu/courses/add/', add_course, name='menu-courses-add'),
+    path('landing/', landing_view, name='landing'),
 
-    path('reservation/', reservation_view, name='cafe-reservation'),
+    path('reservation/', reservation_view, name='reservation'),
 
-    path('a/add/', CourseCreateView.as_view(), name='cafe-reservation'),
+    path('a/add/', CourseCreateView.as_view(), name='reservation'),
 ]
