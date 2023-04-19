17de28da7bc2c58f751274841ca57ea6bed8e80b
Arya <aryatahery@gmail.com>
2023-04-20 04:00:44 +0430

    add to_json method and add_to_course_count method to CartItem

--- cafe/models/cart.py
+++ cafe/models/cart.py
@@ -16,3 +16,17 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     course_count = models.PositiveSmallIntegerField(null=False, blank=False, default=1, max_length=3)
+
+    def add_to_course_count(self):
+        self.course_count += 1
+        self.save()
+
+    def to_json(self):
+        return {
+            'course_count': self.course_count,
+            'name': self.course.name,
+            'price': self.course.price,
+            'total_price': self.course.price * self.course_count,
+            'image_url': self.course.image.url
+
+        }
