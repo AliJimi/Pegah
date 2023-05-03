8d97e27120b8bc02e96b98d244ca8100f0f056d9
Arya <aryatahery@gmail.com>
2023-05-04 00:37:23 +0430

    add new tables

--- cafe/admin.py
+++ cafe/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from cafe.models.course import Material, Course, ColdDrink, Coffee, Milkshake, HotDrink, Dessert, Breakfast, \
     MainCourse, Appetizer, Tea, Smoothie, Salad
-from cafe.models.payment import Payment, PaymentReserve
+from cafe.models.payment import Payment, PaymentReserve, PaymentCourse
 from cafe.models.reservation import Reserve
 from cafe.models.table import Table
+from cafe.models.cart import Cart, CartItem
 
 
 admin.site.register(Material)
@@ -24,3 +25,6 @@ admin.site.register(Payment)
 admin.site.register(PaymentReserve)
 admin.site.register(Reserve)
 admin.site.register(Table)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(PaymentCourse)
